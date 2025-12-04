9916bc9045e74c8f1e8fee88cc38bd30dc0930f5
ramu <ramusriram004@gmail.com>
2025-12-04 12:40:05 +0530

    added support for negative values and few more bug fixes

--- Demo.java
+++ Demo.java
@@ -1,79 +1,82 @@
 import java.util.Arrays;
 import java.util.HashSet;
-class CustomHashSet{
-    // How does an HashSet work? It basically works on the principle of hashing and Hashtables. 
-    // Hash = Takes input, gives output, but here. the goal of an hash function is to generate an integer as an hash value because we are going to use it as an index value
-    // For simplicity, we are going to keep our current Custom HashSet class only allow integers to be used to store the elements in our hashset.
+
+class CustomHashSet {
+    // How does an HashSet work? It basically works on the principle of hashing and
+    // Hashtables.
+    // Hash = Takes input, gives output, but here. the goal of an hash function is
+    // to generate an integer as an hash value because we are going to use it as an
+    // index value
+    // For simplicity, we are going to keep our current Custom HashSet class only
+    // allow integers to be used to store the elements in our hashset.
     int dataArray[];
     int capacity = 0;
     int count = 0;
-    CustomHashSet(int capacity){
+
+    CustomHashSet(int capacity) {
         dataArray = new int[capacity];
         Arrays.fill(dataArray, Integer.MIN_VALUE);
         this.capacity = capacity;
     }
-    
-    void add(int elem){
-        int hash = hashFunc(elem, capacity);
-        int curPointer = hash;
-        while (curPointer < capacity*2) { // why not apply the same logic that we used in contains method? it's smart to just check until capacity*2
-            if(dataArray[curPointer % capacity] == Integer.MIN_VALUE || dataArray[curPointer % capacity] == elem){
-                dataArray[curPointer % capacity] = elem;
+
+    void add(int elem) {
+        int hash = baseHash(elem, capacity);
+        for (int i = 0; i < capacity; i++) {
+            int probe = (hash + i) % capacity;
+            if (dataArray[probe] == Integer.MIN_VALUE) {
+                dataArray[probe] = elem;
                 this.count++;
                 return;
+            } else if (dataArray[probe] == elem) {
+                return;
             }
-            curPointer++;
         }
-        System.out.println("No empty slot found inserting your element " + elem);
+        System.out.println("No empty slot found inserting your element: " + elem);
     }
 
-    boolean contains(int num){
-        if(dataArray[hashFunc(num, capacity)] == num){ 
+    boolean contains(int num) {
+        int hash = baseHash(num, capacity);
+        if (dataArray[hash] == num) { 
             return true;
-        }
-        else{ //the linear probing part 
-            int curPointer = hashFunc(num, capacity);
-            while(curPointer < capacity*2){ //I am doing capacity*2 here cuz I know that if the curPointer has reached beyond that, we didn't find that element anywhere
-                if(dataArray[curPointer%capacity] == num){
+        } else { // the linear probing part
+            for (int i = 1; i < capacity; i++) { //I know that I can completely remove if else and just keep the loop with i starting as 0, but again just for my own understanding when I look back the code.
+                int probe = (hash + i) % capacity;
+                if (dataArray[probe] == num ) {
                     return true;
+                } else if (dataArray[probe] == Integer.MIN_VALUE) {
+                    return false;
                 }
-                curPointer++;
             }
         }
         return false;
     }
 
-    int hashFunc(int elem, int capacity){
+    int baseHash(int elem, int capacity) { //  I know that we don't have to mention capacity parameter always as that property already exists in that class, but I just want keep it as is cuz when I relook the solution, I would understand it intuitively.
         int result = elem % capacity;
-        // //Linear Probing // Making the linear probing logic as method specific, else our code would turn into a bug zoo lol
-        // if(dataArray[result] != Integer.MIN_VALUE){
-        //     int curPointer = result;
-        //     while (curPointer < capacity*2) { // why not apply the same logic that we used in contains method? it's smart to just check until capacity*2
-        //         if(dataArray[curPointer % capacity] == Integer.MIN_VALUE || dataArray[curPointer % capacity] == elem){
-        //             return curPointer%capacity;
-        //         }
-        //         curPointer++;
-        //     }
-        //     return Integer.MIN_VALUE;
-        // }
+        if (result < 0) {
+            return result + capacity;
+        }
         return result;
     }
-    int getSize(){
+
+    int getSize() {
         return count;
     }
+
     @Override
     public String toString() {
         // return super.toString();
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<dataArray.length; i++){
-            if(dataArray[i] != Integer.MIN_VALUE){
+        for (int i = 0; i < dataArray.length; i++) {
+            if (dataArray[i] != Integer.MIN_VALUE) {
                 sb.append(dataArray[i] + ":" + i + " ");
             }
         }
         return sb.toString();
     }
 }
-class Demo{
+
+class Demo {
     public static void main(String[] args) {
         HashSet<Integer> set = new HashSet<>();
         set.add(1);
@@ -86,6 +89,7 @@ class Demo{
         mySet.add(24);
         mySet.add(43);
         mySet.add(46);
+        mySet.add(48);
         mySet.add(48);
         mySet.add(1);
         mySet.add(2);
